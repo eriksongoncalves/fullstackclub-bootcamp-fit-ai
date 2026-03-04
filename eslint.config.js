bfb5de779fc847df9ee1467f7e50e5ff31bf02fe
Erikson Gonçalves Sanchez <eriksongoncalves@yahoo.com.br>
2026-03-04 11:51:27 -0300

    feat: config prisma and schemas

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,17 @@
 import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
-import globals from "globals";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    languageOptions: { globals: globals.node },
     plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node },
   },
-  js.configs.recommended,
   tseslint.configs.recommended,
   eslintConfigPrettier,
   {
