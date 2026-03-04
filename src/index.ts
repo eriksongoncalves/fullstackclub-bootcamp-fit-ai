8bd99a6d2901e8277012dbe6cb55b1cb70d30479
Erikson Gonçalves Sanchez <eriksongoncalves@yahoo.com.br>
2026-03-04 08:22:09 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello, world!");
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+try {
+  await fastify.listen({ port: Number(process.env.PORT) || 8081 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
